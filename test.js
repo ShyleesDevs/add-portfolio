1444e286654497f4b86d8ea7166dd7f1261c8c64
Shailesh Thakur <110343920+shaileshthakur1@users.noreply.github.com>
2023-11-04 18:29:28 +0530

    Create test.js & added my portfolio
    Signed-off-by: Shailesh Thakur  <110343920+shaileshthakur1@users.noreply.github.com>

---
+++ test.js
@@ -0,0 +1,11 @@
+ // Create the <portfolio> element
+var link = document.createElement("portfolio");
+
+// Set the href attribute (the URL)
+link.href = "https://www.linkedin.com/in/shailesh-kumar-thakur-991316205/";
+
+// Set the inner text or HTML of the link
+link.innerText = "My Portfolio"; // or link.innerHTML = "Click here";
+
+// Append the link to the document body or any other element
+document.body.appendChild(link);
